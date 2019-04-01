d9c8639045637955f08400be8e408c744a9ce776
Clayn <clayn_osmato@gmx.de>
2019-04-01 16:35:26 +0200

    feature
    Added the configuration classes
    Added some configuration values to parse between some types
    Added the key for the REST endpoint configuration
    
    Task #3 - Make the Client application configurable

---
+++ Dentahl4J/dentahl4j-fx/src/main/java/net/bplaced/clayn/d4j/config/DentahlConfiguration.java
@@ -0,0 +1,99 @@
+package net.bplaced.clayn.d4j.config;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Properties;
+
+/**
+ *
+ * @author Clayn <clayn_osmato@gmx.de>
+ * @since 0.1
+ */
+public class DentahlConfiguration
+{
+
+    private static final Path DEFAULT_LOCATION = new File("dentahl.properties").toPath();
+    private static final DentahlConfiguration CONFIG = new DentahlConfiguration();
+
+    private final Properties properties = new Properties();
+
+    private DentahlConfiguration()
+    {
+    }
+
+    public static DentahlConfiguration getConfiguration()
+    {
+        return CONFIG;
+    }
+
+    public void load(InputStream in) throws IOException
+    {
+        properties.clear();
+        properties.load(in);
+    }
+
+    public void load() throws IOException
+    {
+        if (!Files.exists(DEFAULT_LOCATION))
+        {
+            return;
+        }
+        try (InputStream in = Files.newInputStream(DEFAULT_LOCATION))
+        {
+            load(in);
+        }
+    }
+
+    public void store(OutputStream out) throws IOException
+    {
+        properties.store(out, "");
+        out.flush();
+    }
+
+    public void store() throws IOException
+    {
+        if (!Files.exists(DEFAULT_LOCATION))
+        {
+            Files.createFile(DEFAULT_LOCATION);
+        }
+        try (OutputStream out = Files.newOutputStream(DEFAULT_LOCATION))
+        {
+            store(out);
+        }
+    }
+
+    public void set(String key, String val)
+    {
+        properties.setProperty(key, val);
+    }
+
+    public String get(String key, String def)
+    {
+        return properties.getProperty(key, def);
+    }
+
+    public String get(String key)
+    {
+        return properties.getProperty(key, null);
+    }
+
+    public <T> void set(Configuration<T> key, T val)
+    {
+        set(key.getKey(), key.toString(val));
+    }
+
+    public <T> T get(Configuration<T> key, T def)
+    {
+        String val = get(key.getKey());
+        return val == null ? def : key.fromString(val);
+    }
+
+    public <T> T get(Configuration<T> key)
+    {
+        return get(key, null);
+    }
+}
---
+++ Dentahl4J/dentahl4j-fx/src/main/java/net/bplaced/clayn/d4j/config/IntegerConfiguration.java
@@ -0,0 +1,29 @@
+package net.bplaced.clayn.d4j.config;
+
+/**
+ *
+ * @author Clayn <clayn_osmato@gmx.de>
+ * @since 0.1
+ */
+public class IntegerConfiguration extends Configuration<Integer>
+{
+
+    public IntegerConfiguration(String key)
+    {
+        super(key);
+    }
+
+    @Override
+    public String toString(Integer object)
+    {
+        return object == null ? "" : object.toString();
+    }
+
+    @Override
+    public Integer fromString(String string)
+    {
+        return string == null || string.isEmpty() ? null : Integer.valueOf(
+                string);
+    }
+
+}
